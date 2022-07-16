8a223e6fb8526c52ba87d5cec591e18bd091ac3f
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-16 09:29:04 +0300

    navbar component

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 import "./Navbar.css";
 
 const Navbar = () => {
-    
+
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='top-bar unclicked'>
+            <div className="bars"></div>
+            <div className="bars"></div>
+            <div className="bars"></div>
+        </div>
+        <div className='side-bar'>
+            <div>Home</div>
+            <div>About</div>
+            <div>Contact</div>
+        </div>
+    </div>
   )
 }
 
