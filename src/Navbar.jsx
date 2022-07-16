861f95fa40a987630432bde42256ac26c80d927c
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-16 10:22:00 +0300

    end

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
 
-const Navbar = () => {
+export default function Navbar() {
 
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [menu_class, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const updateMenu = () => {
+  
+    if (!isMenuClicked) {
+       
+      setBurgerClass("burger-bar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("burger-bar unclicked");
+      setMenuClass("menu hidden");
+    
+    }
+    setIsMenuClicked(!isMenuClicked);
+    
+  };
   return (
-    <div className='navbar'>
-        <div className='top-bar unclicked'>
-            <div className="bars"></div>
-            <div className="bars"></div>
-            <div className="bars"></div>
+    <div style={{ width: "100%", height: "100vh" }}>
+      <nav>
+        <div onClick={updateMenu} className="burger-menu">
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
         </div>
-        <div className='side-bar'>
-            <div>Home</div>
-            <div>About</div>
-            <div>Contact</div>
-        </div>
+      </nav>
+      <div className={menu_class}>
+        <h3> Home</h3>
+        <h3>About</h3>
+        <h3>Contact</h3>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Navbar
